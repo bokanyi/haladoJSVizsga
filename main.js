ce38a7e0db51a092c287409c4bbd7cabed161773
bokanyi <bokanyimi@gmail.com>
2022-01-19 18:32:42 +0100

    findOldestCar függvény a legöregebb auto megkeresése

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//minden reszfeladatban kulon fuggvenyben adjuk meg a fekadatokat
+
 class Project {
     oldestCarBtn;
     after2004Btn;
@@ -11,6 +13,8 @@ class Project {
         this.searchField = document.getElementById('searchField');
         this.searchCarBrandBtn = document.getElementById('searchCarBrandBtn');
         this.resultTbody = document.getElementById('resultTbody');
+    
+        this.oldestCarBtn.onclick = this.findOldestCar;
     }
 
     requestCars = async () => {
@@ -27,11 +31,34 @@ class Project {
             `<tr>
                 <td>${carData.brand}</td>
                 <td>${carData.type}</td>
-                <td>${carData.favtoryYear}</td>
+                <td>${carData.factoryYear}</td>
             </tr>`
         }
         this.resultTbody.innerHTML = resultHTML;
+
+
+    }
+    findOldestCar = async () => {
+        let cars = await this.requestCars();
         
+        let minYear = cars[0].factoryYear;
+        for(let i=1; i<cars.length; i++){
+            if(cars[i].factoryYear < minYear){
+                minYear = cars[i].factoryYear;
+            }
+        } 
+//csinalunk egz ures tombot ezt majd odaadjuk a putcasrontable fuggvenynek
+
+        let result = [];
+        for(let carData of cars){
+            if(carData.factoryYear == minYear){
+                //a push sal hozzaadjuk a holtverseny helyzeteket
+                result.push(carData);
+            }
+        }
+// odaadjuk a putcasrontable fuggvenynek
+        this.putCarsToTable(result);
+
 
     }
 }
